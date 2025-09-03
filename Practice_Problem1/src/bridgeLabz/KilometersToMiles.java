76ff77a8533be14f7a383dce14fe3f3b250f4214
Alok9809 <tushar14746@gmail.com>
2025-09-03 11:41:21 +0530

    Removed all files from assesment branch

--- Practice_Problem1/src/bridgeLabz/KilometersToMiles.java
+++
@@ -1,5 +0,0 @@
-package bridgeLabz;
-
-public class KilometersToMiles {
-
-}
--- Practice_Problem1/src/bridgeLabz/WelcomeToBridgelabz.java
+++
@@ -1,8 +0,0 @@
-package bridgeLabz;
-
-public class WelcomeToBridgelabz {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Bridgelabz!");
-	}
-
-}
